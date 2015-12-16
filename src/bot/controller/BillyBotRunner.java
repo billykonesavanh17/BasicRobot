cc93b41e977bb4e640823f4e4ca8f76641ac3f0e
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-12-16 08:48:09 -0700

    Created robot project

---
+++ src/bot/controller/BillyBotRunner.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package bot.controller;
+
+/**
+ * @author Billy Konesavanh
+ *
+ */
+public class BillyBotRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+
+	}
+
+}
